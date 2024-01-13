1a96c4ef68037add55dbf580c70df9fe1b2a1cef
loidinhm31 <loidinh.git@gmail.com>
2024-01-13 15:10:16 +0700

    Add unstructured data case

---
+++ log_file/log_file.py
@@ -0,0 +1,29 @@
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import regexp_extract, substring_index
+
+
+if __name__ == "__main__":
+    spark = SparkSession \
+        .builder \
+        .master("local[3]") \
+        .appName("log_file") \
+        .getOrCreate()
+
+    file_df = spark.read.text("./apache_logs.txt")
+    file_df.printSchema()
+
+    log_reg = r'^(\S+) (\ S+) (\S+) \[([\w:/]+\s[+\-]\d{4})\] "(\S+) (\S+) (\S+)" (\d{3}) (\S+) "(\S+)" "([^"]*)'
+
+    logs_df = file_df.select(regexp_extract('value', log_reg, 1).alias('ip'),
+                             regexp_extract('value', log_reg, 4).alias('date'),
+                             regexp_extract('value', log_reg, 6).alias('request'),
+                             regexp_extract('value', log_reg, 10).alias('referrer'))
+
+    logs_df.printSchema()
+
+    # Only get domain, not get referrer field with '-'
+    logs_df.withColumn("referrer", substring_index("referrer", "/", 3)) \
+        .where("trim(referrer) != '-' ") \
+        .groupBy("referrer") \
+        .count() \
+        .show(100, truncate=False)
